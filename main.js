99b845a5cf6e57e2d25f8d409357e63b7da3592e
salvatore tullo <salvatoretullo15@gmail.com>
2026-03-02 00:24:04 +0100

    endpoint and emailList

---
+++ main.js
@@ -0,0 +1,6 @@
+//creiamo la variabile per il link della chiamate api
+const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail"
+//qui richiamiamo l'elemento email list con getelementbyid
+const emailListEl = document.getElementById("email_list")
+
+//qui utilizzeremo il metodo con fetch
