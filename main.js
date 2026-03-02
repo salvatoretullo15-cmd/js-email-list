de9ba4ffe9a23ac9d3f95411955ee0429d9487a0
salvatore tullo <salvatoretullo15@gmail.com>
2026-03-02 01:27:36 +0100

    creazione della funzione denerate email con fetch

--- main.js
+++ main.js
@@ -2,5 +2,24 @@
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail"
 //qui richiamiamo l'elemento email list con getelementbyid
 const emailListEl = document.getElementById("email_list")
-
+const buttonEl = document.getElementById("btn")
 //qui utilizzeremo il metodo con fetch
+//creiamo una funzione di generazione delle mail
+function generateEmail() {
+    //utilizziamo un ciclo for per generare dieci mail
+    for (let i = 0; i < 10; i++) {
+      //apriamo una funzione fetch
+      fetch(endpoint)
+        //questo then ci tradurra il il contenuto del link in linguaggio js
+        .then(response => response.json())
+        //con questo invece lo si lavora
+        .then((data) => {
+            //destrutturazione dell'object
+            const { response } = data
+            //creazione della variabile markup con un template litteral con response
+            const markup = `<li>${response}<li>`
+            //trascriviamo il markup creato dentro ul
+            emailListEl.innerHTML += markup
+        })
+    }
+}
